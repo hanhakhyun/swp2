027a01a67445dc81dc33861550f2ce0b22a0cbf6
hanhakhyun <hakhyeon1707@gmail.com>
2025-09-28 22:51:36 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,33 @@
+#define PIN_LED 7
+unsigned int count, toggle, sec;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+ 
+   toggle = 1;
+   count = 0;
+   sec = 1000;
+}
+
+void loop() {
+  if(count < 12) {
+    toggle = toggle_state(toggle);
+    digitalWrite(PIN_LED, toggle);
+    delay(sec);
+    sec = sec_state(sec);
+    count++;
+  }
+  else {
+    while(1){
+      
+      }
+    }
+}
+
+int toggle_state(int toggle) {
+  return !toggle;
+  }
+
+ int sec_state(int sec) {
+  return 300;
+  }
